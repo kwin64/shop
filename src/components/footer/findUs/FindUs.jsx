e2205d990ba0db785e94d6e99622632e12907176
kwin64 <kwin11bvb@yandex.by>
2022-09-19 16:12:26 +0300

    created map for findUs component (not working, need api key googleMapsApi)

--- src/components/footer/findUs/FindUs.jsx
+++ src/components/footer/findUs/FindUs.jsx
@@ -1,11 +1,23 @@
+import { Map } from '../map/GoogleMap';
+import { useJsApiLoader } from '@react-google-maps/api';
 import './FindUs.scss';
 
+const defaultCenter = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
 const FindUs = ({ dataFindUs }) => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'YOUR_API_KEY',
+  });
+
   return (
     <div className="findUs">
       <h1 className="title">{dataFindUs.title}</h1>
       <div className="description">
-        <div className="map">map</div>
+        <div className="map">{isLoaded ? <Map center={defaultCenter} /> : 'Error loading'}</div>
         <div className="contacts">
           {dataFindUs.contacts.map((contacts, index) => {
             return <p key={index}>{contacts}</p>;
---
+++ src/components/footer/map/GoogleMap.jsx
@@ -0,0 +1,26 @@
+import { GoogleMap } from '@react-google-maps/api';
+import { useCallback, useRef } from 'react';
+import './GoogleMap.scss';
+
+const containerStyle = {
+  width: '100%',
+  height: '155px',
+};
+
+export const Map = ({ center }) => {
+  const mapRef = useRef(undefined);
+
+  const onLoad = useCallback(function callback(map) {
+    mapRef.current = map;
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    mapRef.current = undefined;
+  }, []);
+
+  return (
+    <div className="">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}></GoogleMap>
+    </div>
+  );
+};

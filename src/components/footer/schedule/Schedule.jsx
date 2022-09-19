27f94cb4efddb4727a4f68461f5fc595df839f5e
kwin64 <kwin11bvb@yandex.by>
2022-09-19 12:34:14 +0300

    schedule css (all init  css for footer)

--- src/components/footer/schedule/Schedule.jsx
+++ src/components/footer/schedule/Schedule.jsx
@@ -2,11 +2,11 @@ import './Schedule.scss';
 
 const Schedule = ({ dataSchedule }) => {
   return (
-    <div className="schedule">
+    <div className="wrapperSchedule">
       <h1 className="title">{dataSchedule.title}</h1>
       {dataSchedule.days.map((days, index) => {
         return (
-          <div key={index}>
+          <div className="schedule" key={index}>
             <p>{days}</p>
           </div>
         );

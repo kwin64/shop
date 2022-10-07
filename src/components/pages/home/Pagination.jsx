adca2967cd7244b80f6b82079a2467f2801a70a3
kwin64 <kwin11bvb@yandex.by>
2022-10-07 13:27:24 +0300

    css pagination page

--- src/components/pages/home/Pagination.jsx
+++ src/components/pages/home/Pagination.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const Pagination = ({ pages, setCurrentPage }) => {
+  const [activePage, setActivePage] = React.useState(0);
+
+  const onClickHandler = (page, index) => {
+    setCurrentPage(page);
+    setActivePage(index);
+  };
   return (
     <div className="containerPagination">
       {pages.map((page, index) => {
         return (
-          <div key={index} className={'page'} onClick={() => setCurrentPage(page)}>
+          <div
+            key={index}
+            className={activePage === index ? 'active' : 'page'}
+            onClick={() => onClickHandler(page, index)}>
             {page + 1}
           </div>
         );
